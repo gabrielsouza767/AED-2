62306a23ce4281a6ae856dd6de9640dfb40ce522
Gabriel souza <gabrielsouza7155@gmail.com>
2025-11-24 17:52:54 -0300

    Fiz a tela inicial

--- TrabalhoABB/src/main.c
+++ TrabalhoABB/src/main.c
@@ -5,26 +5,42 @@
 int main(int argc, char const *argv[])
 {
     FILE *fp = fopen(argv[1], "r");
-    int quantidade = QuantPessoas(fp) , idade;
+    int quantidade = QuantPessoas(fp), idade, flag = 0;
+    char escolha;
     ABB tree;
     tree.raiz = NULL;
     Pessoa p;
-    No *pessoaEncontrada= NULL;
+    No *pessoaEncontrada = NULL;
     LeArquivo(fp, &p, &tree);
-    printf("Digite a idade de quem desejas buscar: ");
-    scanf("%d", &idade);
-    pessoaEncontrada = buscaABB(tree.raiz, idade);
-    if (pessoaEncontrada != NULL)
+    do
     {
-       printf("----------------------------------------\n");
-       printf("Pessoa encontrada! \n");
-       printf("----------------------------------------\n");
-       printf("Nome: %s \n",  pessoaEncontrada->dado.nome);
-       printf("Idade: %d \n",  pessoaEncontrada->dado.idade);
-       printf("Altura: %.2f \n",  pessoaEncontrada->dado.altura);
-    }
-    else
-        printf("Pessoa não encontrada! \n");
+        printf("Digite a idade de quem desejas buscar: ");
+        scanf("%d", &idade);
+        pessoaEncontrada = buscaABB(tree.raiz, idade);
+        if (pessoaEncontrada != NULL)
+        {
+            printf("----------------------------------------\n");
+            printf("Pessoa encontrada! \n");
+            printf("----------------------------------------\n");
+            printf("Nome: %s \n", pessoaEncontrada->dado.nome);
+            printf("Idade: %d \n", pessoaEncontrada->dado.idade);
+            printf("Altura: %.2f \n", pessoaEncontrada->dado.altura);
+            printf("----------------------------------------\n");
+            printf("Deseja remove-la? (s/n)");
+            scanf("%c ", &escolha);
+            if (escolha == 's' || escolha == 'S')
+                remove();
+            else
+                printf("Muito obrigado!");
+        }
+        else
+            printf("Pessoa não encontrada! \n");
+        printf("Deseja buscar mais alguem? (s/n)");
+        scanf(" %c", &escolha);
+        if (escolha == 'n' || escolha == 'N')
+            flag = 1;
+        
+    } while (!flag);
     fclose(fp);
     return 0;
 }
